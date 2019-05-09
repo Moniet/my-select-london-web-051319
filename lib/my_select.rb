0f59e34db6bff993a1be014454488b153b9d3743
moniet <moniet.sawhney@gmail.com>
2019-05-09 11:06:43 +0100

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
-def my_select(collection)
- # your code here!
+def my_select(arr)
+  new_arr = []
+  i = 0
+  while i < arr.length
+    val = yield(arr[i])
+    if val
+      new_arr << arr[i]
+    end
+    i+=1
+  end
+  new_arr
 end
